88e36598c76758047c8c4f239a756144daf9f8ca
IbrahimZahidd <232098157+MuhammadIbrahimZahid@users.noreply.github.com>
2026-07-03 22:56:48 +0500

    Third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,19 +29,19 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-1ZCMGK2M6Y"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
 
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
-          gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
 
-          gtag('config', 'G-1ZCMGK2M6Y');
-        `}
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+  `}
       </Script>
 
       <body className="min-h-full flex flex-col">{children}</body>
