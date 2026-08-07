c345a302e33b0af548b9e2232214f3462f304040
IbrahimZahidd <232098157+MuhammadIbrahimZahid@users.noreply.github.com>
2026-08-07 21:33:50 +0500

    Phase-7 ongoing fixing

--- src/services/adminLeads.ts
+++ src/services/adminLeads.ts
@@ -1,8 +1,16 @@
 import pool from "@/lib/db";
 
 export async function getAdminLeads() {
-  const result = await pool.query(
-    `
+  const result = await pool.query(`
+    SELECT
+      MAX(id) AS latest_id,
+      COUNT(*) AS total
+    FROM public.leads;
+  `);
+
+  console.log("DB INFO:", result.rows[0]);
+
+  const leads = await pool.query(`
     SELECT
       id,
       first_name,
@@ -14,8 +22,9 @@ export async function getAdminLeads() {
       updated_at
     FROM public.leads
     ORDER BY created_at DESC;
-    `,
-  );
+  `);
 
-  return result.rows;
+  console.log("TOP LEAD:", leads.rows[0]);
+
+  return leads.rows;
 }
